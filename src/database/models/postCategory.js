4ce3ce6c3eeee4aca211c4d5e275c5fc9cd61ad5
klauslube <klauslube93@gmail.com>
2022-08-25 16:57:51 -0300

    feat: criado model postCategory

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: { type: DataTypes.INTEGER, primaryKey: true }
+  },
+  {
+    categoryId: { type: DataTypes.INTEGER, primaryKey: true }
+  },
+  {
+    timestamps: false,
+    tableName: 'Categories'
+  });
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blogPosts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+
+  return PostCategory;
+};
