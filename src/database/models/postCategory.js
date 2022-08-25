ed5c7c77078b538812cfe33a7dc56812003f478e
klauslube <klauslube93@gmail.com>
2022-08-25 17:15:04 -0300

    fix: correcao de chaves

--- src/database/models/postCategory.js
+++ src/database/models/postCategory.js
@@ -1,8 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
   const PostCategory = sequelize.define('PostCategory', {
-    postId: { type: DataTypes.INTEGER, primaryKey: true }
-  },
-  {
+    postId: { type: DataTypes.INTEGER, primaryKey: true },
     categoryId: { type: DataTypes.INTEGER, primaryKey: true }
   },
   {
